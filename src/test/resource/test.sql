780317484d6b93cec4e83aa1c3e80e47b9dd934e
Yuliia-Sh <uliya1301@rambler.ru>
2020-02-16 22:14:07 +0200

    Added CREATE TABLE st

--- src/test/resource/test.sql
+++ src/test/resource/test.sql
@@ -1,2 +1,19 @@
+
+CREATE TABLE users(
+id integer PRIMARY KEY,
+first_name text,
+last_name text,
+salary integer
+);
 INSERT INTO users(id, first_name, last_name, salary)
-VALUES (20, 'Юлия','Семеренко',300);
+VALUES (1, 'Yuliia','Semerenko',320);
+INSERT INTO users(id, first_name, last_name, salary)
+VALUES (2, 'Yuliia','Kemerenko',330);
+INSERT INTO users(id, first_name, last_name, salary)
+VALUES (3, 'Ivan','Remerenko',304);
+INSERT INTO users(id, first_name, last_name, salary)
+VALUES (4, 'Yuliia','Semerenko',360);
+INSERT INTO users(id, first_name, last_name, salary)
+VALUES (5, 'Yuliia','Semerenko',580);
+INSERT INTO users(id, first_name, last_name, salary)
+VALUES (20, 'Yuliia','Serenko',300);
